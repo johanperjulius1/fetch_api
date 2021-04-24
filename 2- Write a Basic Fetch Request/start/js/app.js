0f9fc6993c119b523d9b27b576ca65aa9d53f5c2
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2021-04-24 16:38:45 +0200

    Create a reusable fetch function

--- 2- Write a Basic Fetch Request/start/js/app.js
+++ 2- Write a Basic Fetch Request/start/js/app.js
@@ -1,48 +1,64 @@
-const select = document.getElementById('breeds');
-const card = document.querySelector('.card');
-const form = document.querySelector('form');
-
+const select = document.getElementById("breeds");
+const card = document.querySelector(".card");
+const form = document.querySelector("form");
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
-fetch("https://dog.ceo/api/breeds/image/random")
-  .then(response => response.json())
-  .then(data => generateImage(data.message))
+function fetchData(url) {
+  return fetch(url)
+    .then(res => res.join());
+}
+
+fetchData("https://dog.ceo/api/breeds/list").then((data) =>
+  generateListItems(data.message)
+);
+
+fetchData("https://dog.ceo/api/breeds/image/random").then((data) =>
+  generateImage(data.message)
+);
+
+function generateListItems(data) {
+  const items = data
+    .map(
+      (item) => `
+      <option value='${item}'>${item}</option>
+         `).join("");
+  select.innerHTML = items;
+}
+
 
 function generateImage(data) {
   const html = `
     <img src=${data} alt>
     <p>Click to view image of ${select.value}</p>
-  `
+  `;
   card.innerHTML = html;
 }
 
-fetch("https://dog.ceo/api/breeds/list")
-  .then(response => response.json())
-  .then(data => generateListItems(data.message))
+function fetchBreedImage() {
+  const breed = select.value;
+  const img = card.querySelector("img");
+  const p = card.querySelector("p");
 
-  function generateListItems(data) {
-    const items = data.map(item =>`
-      <option value='${item}'>${item}</option>
-      `
-    )
-    select.innerHTML = items
-  }
+  fetchData(`https://dog.ceo/api/breed/${breed}/images/random`).then((data) => {
+    img.src = data.message;
+    img.alt = "breed";
+    p.textContent = `Click to view more ${breed}s`
+  });
+}
+
+select.addEventListener('change', fetchBreedImage)
+card.addEventListener('click', fetchBreedImage)
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
 
-
-
 // ------------------------------------------
 //  EVENT LISTENERS
 // ------------------------------------------
 
-
-
 // ------------------------------------------
 //  POST DATA
 // ------------------------------------------
-
